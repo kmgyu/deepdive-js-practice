b424788f45263223af9fadbf6cfbc0347f3f6830
kmgyu <lidersy961@gmail.com>
2025-07-21 11:54:06 +0900

    add pages and router

---
+++ src/pages/Tictactoe.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
+import Examples from '../pages/Examples';
+import TicTacToe from '../pages/TicTacToe';
+
+function AppRouter() {
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/examples" element={<Examples />} />
+      <Route path="/TicTacToe" element={<TicTacToe />} />
+      {/* 여기에 다른 페이지 라우트 추가 가능 */}
+    </Routes>
+  );
+}
+
+export default AppRouter;
