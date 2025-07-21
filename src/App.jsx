bee60110b31c31d636e1d9fb83df8a7b568a1a16
kmgyu <lidersy961@gmail.com>
2025-07-21 13:21:31 +0900

    design: header footer 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from '@/routes/AppRouter';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 export default function App() {
   return (
-    // <div className="app-container min-h-screen flex items-center justify-center bg-gray-900 text-white">
+    <>
+    <Header />
+    {/* <div className="app-container min-h-screen flex items-center justify-center bg-gray-900 text-white"> */}
     <BrowserRouter>
       <AppRouter />
     </BrowserRouter>
-    // </div>
+    {/* </div> */}
+    <Footer />
+    </>
   );
 }
