e00785ef3db4e395fea600c6a008d7a3e832bdf0
kmgyu <lidersy961@gmail.com>
2025-07-21 11:54:42 +0900

    update app router

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Dashboard from '../pages/Dashboard';
-import Scenario from '../pages/Scenario';
+import Dashboard from './Dashboard';
+import Examples from './Examples';
+import TicTacToe from './TicTacToe';
 
 function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
-      <Route path="/scenario" element={<Scenario />} />
+      <Route path="/examples" element={<Examples />} />
+      <Route path="/TicTacToe" element={<TicTacToe />} />
       {/* 여기에 다른 페이지 라우트 추가 가능 */}
     </Routes>
   );
